0fd596e12cc6dc6e66688c564813edd37ddc98e8
JhorysGoezR <jhorys.goezr@udea.edu.co>
2025-09-24 17:25:20 -0500

    Se agregaron 2 funciones

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@ unsigned char rotar_derecha(unsigned char byte, int n);
 unsigned char rotar_izquierda(unsigned char byte, int n);
 void XOR(char* Arreglo_Bytes, int tamaño, unsigned char clave);
 void desencriptar_buffer(char* buffer, int tamaño, int n, unsigned char k );
+int es_digito(char caracter);
+int calcular_longitud_cadena(const char* cadena);
+
 
 int main()
 {
@@ -12,6 +15,17 @@ int main()
     return 0;
 }
 
+int calcular_longitud_cadena(const char* cadena){
+    int longitud = 0;
+    while(cadena[longitud] != '\0'){
+        ++longitud;
+    }
+    return longitud;
+}
+
+int es_digito(char caracter){
+    return (caracter >= '0' && caracter <= '9' );
+}
 
 // Función para rotar un byte a la izquierda n posiciones (0 < n < 8)
 unsigned char rotar_izquierda(unsigned char byte, int n) {
@@ -23,9 +37,9 @@ unsigned char rotar_derecha(unsigned char byte, int n) {
     return (byte >> n) | (byte << (8 - n));
 }
 
-void XOR(char* Arreglo_Bytes, int tamaño, unsigned char clave) {
+void XOR(char* buffer, int tamaño, unsigned char clave) {
     for (int i = 0; i < tamaño; i++) {
-        Arreglo_Bytes[i] = (Arreglo_Bytes[i] ^ clave);  // operación XOR bit a bit
+        buffer[i] = (buffer[i] ^ clave);  // operación XOR bit a bit
     }
 }
 
